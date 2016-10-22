6cf616e99763aae60e13ed915d0851e8fc4bc17a
Irsyad <irsyad.muhd@gmail.com>
2016-10-22 23:51:17 +0800

    done setting up

---
+++ public/app.jsx
@@ -0,0 +1,9 @@
+var MenuPage = React.createClass({
+  render: function() {
+    return(
+        <div>Hello world</div>
+    )
+  }
+})
+
+ReactDOM.render(<MenuPage/>, document.getElementById('container'));
