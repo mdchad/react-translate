951d83755614fe94af30d2e577fe880296240e8c
Irsyad <irsyad.muhd@gmail.com>
2016-10-24 01:11:26 +0800

    done greet

--- public/app.jsx
+++ public/app.jsx
@@ -3,11 +3,9 @@ var MenuPage = React.createClass({
     return{
       start: false,
     }
-
   },
 
   information: function() {
-    console.log(this.state.start)
     if(this.state.start){
       this.setState({start: false})
     } else {
@@ -17,14 +15,10 @@ var MenuPage = React.createClass({
 
 
   showInformation: function(start) {
-    console.log("whats happening?")
-
       if(start) {
         return (
-          <div className="wrap">
-            <h1>Topics</h1>
+          <div>
             <Topics />
-            <button onClick={this.information}>back</button>
           </div>)
       } else {
         return (
@@ -33,14 +27,13 @@ var MenuPage = React.createClass({
             <p>This website aims to teach you simple conversational Malay and to impress to your mat/minah friends</p>
             <p><strong>Why learn Malay?</strong></p>
             <p>To get a better understanding of our national language.<br></br> Yeah get a better understanding of what ever the heck you sang for your national anthem.<br></br><small>(and also not to get scammed when in Malaysia)</small></p>
-            <p>This idea came by because of Eugene who kept bugging me to translate every sentence/word to Malay. (and also because of his poor attempt at speaking Malay)</p>
+            <p>This idea came by because of Eugene who kept bugging me to translate every word to Malay. (and also because of his poor attempt at speaking Malay)</p>
             <button onClick={this.information}>Start learning</button>
           </div>
       )}
   },
 
   render: function() {
-    console.log(this.state.start)
 
     return(
       <div>{this.showInformation(this.state.start)}</div>
--- public/topics.jsx
+++ public/topics.jsx
@@ -1,34 +1,61 @@
 var Topics = React.createClass({
-  // getInitialState: function() {
-  //   clickNext: false
-  // },
-  //
-  // nextPage: function() {
-  //   if(this.state.clickNext){
-  //     this.setState({clickNext: false})
-  //   } else {
-  //     this.setState({clickNext: true})
-  //   }
-  // },
+  getInitialState: function() {
+    return {
+      clickNext: false,
+    }
+  },
+
+  translateFirst: function() {
+    if(this.state.clickNext){
+      this.setState({clickNext: false})
+    } else {
+      this.setState({clickNext: true})
+    }
+  },
 
 
 
-
+  translateNow: function(clickNext) {
+    if(clickNext) {
+    return(
+      <div>
+        <button className="back" onClick={this.translateFirst}><i className="fa fa-chevron-left" aria-hidden="true"></i> back</button>
+        <div className="wrap2">
+          <ul className="leftSide">
+            <li>Good morning</li>
+            <li>How are you</li>
+            <li>Goodbye</li>
+            <li>I'm good</li>
+            <li>How's everthing?</li>
+          </ul>
+          <ul className="rightSide">
+            <li>Selamat pagi</li>
+            <li>Amacam ? baik ?</li>
+            <li>Selamat jalan</li>
+            <li>Aku baik</li>
+            <li>Sume okay?</li>
+          </ul>
+        </div>
+      </div>
+    )} else {
+      return (
+      <div className="wrap">
+        <h1>Topics</h1>
+        <ul>
+          <li onClick={this.translateFirst}>Greet</li>
+          <li onClick={this.translateSecond}>Eat</li>
+        </ul>
+      </div>
+    )}
+  },
 
 
 
 
 
   render: function() {
-    console.log('is this working?')
-
     return(
-      <div>
-        <ul>
-          <li>Greet</li>
-          <li>Eat</li>
-          <li>Insult</li>
-        </ul>
+      <div>{this.translateNow(this.state.clickNext)}
       </div>
     )
   }
